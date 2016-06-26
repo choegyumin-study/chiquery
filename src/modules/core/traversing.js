f3bf08526d4566f7daf86d1814e77de554611f74
GyuminChoi <choegyumin@gmail.com>
2016-06-26 22:07:20 +0900

    Design architecture

--- src/modules/core/traversing.js
+++ src/modules/core/traversing.js
@@ -5,6 +5,7 @@ define(function() {
 	var modules = {};
 
 	modules.each = function(elements, callback) {
+		console.log('each');
 		for (i in elements) {
 			var i = 0;
 			if (callback.call(elements[i], i, elements[i]) === false) {
