e19ebfad7a4db32fbeff857648256a9716309127
GyuminChoi <choegyumin@gmail.com>
2016-07-17 15:43:58 +0900

    Convert es5 code to es6

--- src/modules/feature/attr.js
+++ src/modules/feature/attr.js
@@ -8,14 +8,13 @@ export default function() {
 	modules.addClass = (_this, className) => {
 		let element;
 		const len = _this.size();
+		if (len < 1) return _this;
 		if (TOOL_fn().isString(className)) {
-			if (len < 1) return _this;
 			for (var _i = 0; _i < len; _i++) {
 				element = _this.get(_i);
 				if (TOOL_fn().hasAttr(element, "class", className) === false) chiQuery(element).attr("class", `${chiQuery(element).attr("class")} ${className}`);
 			}
 		} else if (TOOL_fn().isFunction(className)) {
-			if (len < 1) return _this;
 			for (var _i = 0; _i < len; _i++) {
 				element = _this.get(_i);
 				if (TOOL_fn().hasAttr(element, "class", className) === false) chiQuery(element).addClass(className.call(element, _i, chiQuery(element).attr("class")));
@@ -27,8 +26,8 @@ export default function() {
 	modules.attr = (_this, attrName, attrValue) => {
 		let element;
 		const len = _this.size();
+		if (len < 1) return _this;
 		if (TOOL_fn().isUndefined(attrValue)) {
-			if (len < 1) return _this;
 			return _this.get(0).getAttribute(attrName);
 		} else if (TOOL_fn().isString(attrValue)) {
 			for (var _i = 0; _i < len; _i++) {
@@ -37,7 +36,6 @@ export default function() {
 			}
 			return _this;
 		} else if (TOOL_fn().isFunction(attrValue)) {
-			if (len < 1) return _this;
 			for (var _i = 0; _i < len; _i++) {
 				element = _this.get(_i);
 				chiQuery(element).attr(attrName, attrValue.call(element, _i, element.getAttribute(attrName)));
@@ -62,8 +60,8 @@ export default function() {
 	modules.prop = (_this, propertyName, propertyValue) => {
 		let element;
 		const len = _this.size();
+		if (len < 1) return undefined;
 		if (TOOL_fn().isObject(propertyName)) {
-			if (len < 1) return undefined;
 			for (var _i = 0; _i < len; _i++) {
 				const propertyObj = Object.keys(propertyName);
 				if (propertyObj.length < 1) return _this;
@@ -75,14 +73,12 @@ export default function() {
 		} else if (TOOL_fn().isUndefined(propertyValue)) {
 			return _this.get(0)[propertyName];
 		} else if (TOOL_fn().isFunction(propertyValue) === false) {
-			if (len < 1) return undefined;
 			for (var _i = 0; _i < len; _i++) {
 				element = _this.get(_i);
 				element[propertyName] = propertyValue;
 			}
 			return _this;
 		} else if (TOOL_fn().isFunction(propertyValue)) {
-			if (len < 1) return undefined;
 			for (var _i = 0; _i < len; _i++) {
 				element = _this.get(_i);
 				chiQuery(element).prop(propertyName, propertyValue.call(element, _i, chiQuery(element).prop(propertyName)));
@@ -105,15 +101,14 @@ export default function() {
 	modules.removeClass = (_this, className) => {
 		let element;
 		const len = _this.size();
+		if (len < 1) return _this;
 		if (TOOL_fn().isString(className)) {
-			if (len < 1) return _this;
 			for (var _i = 0; _i < len; _i++) {
 				element = _this.get(_i);
 				const regex = new RegExp(`(\\s|^)${className}(\\s|$)`);
 				chiQuery(element).attr("class", chiQuery(element).attr("class").replace(regex, " ").trim());
 			}
 		} else if (TOOL_fn().isFunction(className)) {
-			if (len < 1) return _this;
 			for (var _i = 0; _i < len; _i++) {
 				element = _this.get(_i);
 				chiQuery(element).removeClass(className.call(element, _i, chiQuery(element).attr("class")));
@@ -137,8 +132,8 @@ export default function() {
 	modules.toggleClass = (_this, className, status) => {
 		let element;
 		const len = _this.size();
+		if (len < 1) return _this;
 		if (TOOL_fn().isString(className)) {
-			if (len < 1) return _this;
 			if (TOOL_fn().isBoolean(status)) {
 				if (status === true) {
 					chiQuery(element).addClass(className);
@@ -156,7 +151,6 @@ export default function() {
 				}
 			}
 		} else if (TOOL_fn().isFunction(className)) {
-			if (len < 1) return _this;
 			for (var _i = 0; _i < len; _i++) {
 				element = _this.get(_i);
 				chiQuery(element).toggleClass(className.call(element, _i, chiQuery(element).attr("class")));
